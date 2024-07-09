fc003a5d77b095244117854f26c2100c2943d079
danielkosgei <dannykosgei@gmail.com>
2024-07-09 11:21:48 +0300

    added cards for the products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
 
 <body>
+
 
     <div class="container">
         <div class="row">
@@ -20,7 +22,7 @@
                     <div class="card-body">
                       <h5 class="card-title">Card title</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <a href="#" class="btn btn-primary">Add to Cart</a>
                     </div>
                 </div>
 
@@ -45,6 +47,67 @@
                     </div>
                 </div>
             </div>
+            <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('images/p4.jpg') }}" alt="Description of the image">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('images/p5.jpg') }}" alt="Description of the image">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('images/p6.jpg') }}" alt="Description of the image">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('images/p7.jpg') }}" alt="Description of the image">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('images/p8.jpg') }}" alt="Description of the image">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('images/p9.jpg') }}" alt="Description of the image">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
     
