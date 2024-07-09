73eba98f64f3e412082e03114c27b30650c43365
Mercy Wairimu <105119031+MasyWairimu@users.noreply.github.com>
2024-07-09 15:34:34 +0300

    finding difficulty in linking signup page to css file

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ecomm</title>
-    <link rel="stylesheet" href="../css/signup.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/signup.css">
 </head>
 
-<body>
+<body id = "signup-page">
 
     <form>
-        <div class="form-group row">
+        <div class="form-group row" id="name">
             <div class="col"> <label class="form-label" for="customFile">Profile Picture</label> </div>
             <div class="col"> <input type="file" class="form-control" id="customFile" /></div>
         </div>
