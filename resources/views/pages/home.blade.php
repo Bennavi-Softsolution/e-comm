a259321fa42e76928fae03bf3049c6b8c43a2115
danielkosgei <dannykosgei@gmail.com>
2024-07-11 13:41:30 +0300

    improved categories section using controllers

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,22 +1,30 @@
 @extends('layouts.app')
 
 @section('home')
-<div class="container-fluid justify-content-start">
+<style>
+    /* Custom style for active nav-link */
+    .nav-pills .nav-link.active {
+        background-color: red; /* Change background color */
+        color: white; /* Change text color */
+        border-color: red; /* Change border color (optional) */
+    }
+</style>
+
+<div class="container-fluid mt-3">
     <div class="row">
         <!-- Sidebar container (first column) -->
         <div class="col-md-3">
             <div class="container">
-                <ul class="nav flex-column nav-pills">
+                <ul id="myTab" class="nav flex-column nav-pills">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Featured</a>
+                        <a class="nav-link active" aria-current="page" href="#shoes">Shoes</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Top Selling</a>
+                        <a class="nav-link" href="#watches">Watches</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Discounted</a>
+                        <a class="nav-link" href="#accessories">Accessories</a>
                     </li>
-                    
                 </ul>
             </div>
         </div>
@@ -24,37 +32,115 @@
         <!-- Main content container (second column) -->
         <div class="col-md-9">
             <div class="container">
-                <div class="row row-cols-1 row-cols-md-3 g-2">
-                    @foreach ($products as $product)
-                        <div class="col mb-1">
-                            <div class="card h-100">
-                                <img src="{{ asset('images/' . $product->image) }}" class="card-img-top"
-                                    alt="{{ $product->title }}">
-                                <div class="card-body d-flex flex-column">
-                                    <h5 class="card-title">{{ $product->title }}</h5>
-                                    <p class="card-text">{{ $product->description }}</p>
-                                    <div class="row justify-content-between align-items-end mt-auto">
-                                        <div class="rating-stars">
-                                            @for ($i = 0; $i < 5; $i++)
-                                                <i class="fas fa-star {{ $i < $product->rating ? 'checked' : '' }}"></i>
-                                            @endfor
-                                        </div>
-                                        <div class="col-auto">
-                                            <h5>KES {{ number_format($product->price) }}</h5>
-                                        </div>
-                                        <div class="col-auto">
-                                            <a href="#" id="btn-custom" class="btn btn-primary rounded-pill">
-                                                <i class="bi bi-cart-plus"></i> Add to Cart
-                                            </a>
+                <div class="tab-content">
+                    <div id="shoes" class="tab-pane fade show active">
+                        <div class="row row-cols-1 row-cols-md-3 g-2">
+                            @foreach ($shoes as $product)
+                                <div class="col mb-3">
+                                    <div class="card h-100">
+                                        <img src="{{ asset('images/' . $product->image) }}" class="card-img-top"
+                                            alt="{{ $product->title }}">
+                                        <div class="card-body d-flex flex-column">
+                                            <h5 class="card-title">{{ $product->title }}</h5>
+                                            <p class="card-text">{{ $product->description }}</p>
+                                            <div class="row justify-content-between align-items-end mt-auto">
+                                                <div class="rating-stars">
+                                                    @for ($i = 0; $i < 5; $i++)
+                                                        <i class="fas fa-star {{ $i < $product->rating ? 'checked' : '' }}"></i>
+                                                    @endfor
+                                                </div>
+                                                <div class="col-auto">
+                                                    <h5>KES {{ number_format($product->price) }}</h5>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <a href="#" class="btn btn-primary rounded-pill">
+                                                        <i class="bi bi-cart-plus"></i> Add to Cart
+                                                    </a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            @endforeach
                         </div>
-                    @endforeach
+                    </div>
+                    <div id="watches" class="tab-pane fade">
+                        <div class="row row-cols-1 row-cols-md-3 g-2">
+                            @foreach ($watches as $product)
+                                <div class="col mb-3">
+                                    <div class="card h-100">
+                                        <img src="{{ asset('images/' . $product->image) }}" class="card-img-top"
+                                            alt="{{ $product->title }}">
+                                        <div class="card-body d-flex flex-column">
+                                            <h5 class="card-title">{{ $product->title }}</h5>
+                                            <p class="card-text">{{ $product->description }}</p>
+                                            <div class="row justify-content-between align-items-end mt-auto">
+                                                <div class="rating-stars">
+                                                    @for ($i = 0; $i < 5; $i++)
+                                                        <i class="fas fa-star {{ $i < $product->rating ? 'checked' : '' }}"></i>
+                                                    @endfor
+                                                </div>
+                                                <div class="col-auto">
+                                                    <h5>KES {{ number_format($product->price) }}</h5>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <a href="#" class="btn btn-primary rounded-pill">
+                                                        <i class="bi bi-cart-plus"></i> Add to Cart
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    <div id="accessories" class="tab-pane fade">
+                        <div class="row row-cols-1 row-cols-md-3 g-2">
+                            @foreach ($accessories as $product)
+                                <div class="col mb-3">
+                                    <div class="card h-100">
+                                        <img src="{{ asset('images/' . $product->image) }}" class="card-img-top"
+                                            alt="{{ $product->title }}">
+                                        <div class="card-body d-flex flex-column">
+                                            <h5 class="card-title">{{ $product->title }}</h5>
+                                            <p class="card-text">{{ $product->description }}</p>
+                                            <div class="row justify-content-between align-items-end mt-auto">
+                                                <div class="rating-stars">
+                                                    @for ($i = 0; $i < 5; $i++)
+                                                        <i class="fas fa-star {{ $i < $product->rating ? 'checked' : '' }}"></i>
+                                                    @endfor
+                                                </div>
+                                                <div class="col-auto">
+                                                    <h5>KES {{ number_format($product->price) }}</h5>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <a href="#" class="btn btn-primary rounded-pill">
+                                                        <i class="bi bi-cart-plus"></i> Add to Cart
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // Initialize Bootstrap tabs
+        $('#myTab a').on('click', function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+        });
+    });
+</script>
 @endsection
