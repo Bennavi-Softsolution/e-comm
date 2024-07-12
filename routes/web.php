50cda119794a8bd059b6fff54d0e805276f04502
Lista-KM <mngeikerubo@gmail.com>
2024-07-12 13:28:16 +0300

    Well..

--- routes/web.php
+++ routes/web.php
@@ -43,8 +43,7 @@ Route::get('/signup', function () {
 });
 Route::get('login', function () {
     return view('login');
-
+});
 Route::get('/payment', function () {
     return view('pages/payment');
-});
 });
