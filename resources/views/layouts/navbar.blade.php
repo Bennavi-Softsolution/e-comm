3aea3faa7363ba97c42c068f9919be62b2e623b4
danielkosgei <dannykosgei@gmail.com>
2024-07-16 10:03:13 +0000

    login page redesigned into modal

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,32 +2,24 @@
 
 
 <style>
-    .navbar-custom {
-        background-color: #D9D9D9 !important;
-        border-radius: 10px;
-        margin: 10px;
-    }
+    
 
     .navbar-brand span {
         color: #EE1313;
     }
 
-    .input-group .form-control {
-        border-radius: 10px 0 0 10px !important;
-        background-color: white;
-    }
+    
 
     .input-group .btn-search {
-        background-color: black;
+        
         border-radius: 0 10px 10px 0 !important;
-        color: #EE1313;
+        
         border: none;
         /* Remove the border */
     }
 
     .navbar .icons a {
-        margin-left: 10px;
-        color: #EE1313;
+        
     }
 
     .from-custom {
@@ -41,19 +33,23 @@
 
 
 <!--Main Contenet-->
-<nav class="navbar navbar-expand-lg navbar-custom">
+<nav class="navbar navbar-expand-lg border-bottom border-body">
     <div class="container">
         <a class="navbar-brand" href="#">
             E-<span>Commerce</span>.site
         </a>
         <div class="input-group from-custom mx-auto">
             <input class="form-control " type="search" placeholder="I am shopping for..." aria-label="Search">
-            <button class="btn btn-search" type="button">
+            <button class="btn btn-custom btn-search" type="button">
                 <i class="fas fa-search"></i>
             </button>
         </div>
         <div class="icons icon-custom">
             <a href="#"><i class="fas fa-user"></i></a>
+            
+        </div>
+        <div class="icons icon-custom">
+            
             <a href="#"><i class="fas fa-shopping-cart"></i></a>
         </div>
     </div>
